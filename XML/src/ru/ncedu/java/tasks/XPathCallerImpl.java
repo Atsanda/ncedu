14a1698be4032ac1467cdb92305135a866350c0d
Artyom <tsanda.artyom@gmail.com>
2015-10-10 19:45:29 +0300

    some functions implemented in xml

--- XML/src/ru/ncedu/java/tasks/XPathCallerImpl.java
+++ XML/src/ru/ncedu/java/tasks/XPathCallerImpl.java
@@ -63,34 +63,99 @@ public class XPathCallerImpl implements  XPathCaller {
 
     @Override
     public String getHighestPayed(Document src, String deptno, String docType) {
-        return null;
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression = ".//employee[@deptno = '"+deptno+"' and sal = (//employee[@deptno = '"+deptno+"']/sal[not(. < //employee[@deptno = '"+deptno+"']/sal)])[1]]";
+        Node node = null;
+
+        try {
+            node = (Node) xPath.compile(expression).evaluate(src, XPathConstants.NODE);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+
+        return ((Element) node).getElementsByTagName("ename").item(0).getTextContent();
     }
 
     @Override
     public Element[] getTopManagement(Document src, String docType) {
-        return new Element[0];
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression;
+        if(docType.equals("emp-hier"))
+            expression = "//employee[count(ancestor::*) = 0]";
+        else
+            expression = "//employee[not(@mgr)]";
+
+        NodeList nodeList = null;
+
+        try {
+            nodeList = (NodeList) xPath.compile(expression).evaluate(src, XPathConstants.NODESET);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+
+        return conversion(nodeList);
     }
 
     @Override
     public Element[] getOrdinaryEmployees(Document src, String docType) {
-        return new Element[0];
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression;
+        if(docType.equals("emp-hier"))
+            expression = "//employee[not(./employee)]";
+        else
+            expression = "//employee[not(@empno = (//@mgr))]";
+
+        NodeList nodeList = null;
+
+        try {
+            nodeList = (NodeList) xPath.compile(expression).evaluate(src, XPathConstants.NODESET);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+
+        return conversion(nodeList);
     }
 
     @Override
     public Element[] getCoworkers(Document src, String empno, String docType) {
-        return new Element[0];
+
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+
+        String expression;
+        if(docType.equals("emp-hier"))
+            expression = "//employee[@empno = '" + empno + "']/ancestor::*[1]/child::employee";
+        else
+            expression = "//employee[@mgr = //employee[@empno = //employee[@empno = '" + empno + "']/@mgr]/@empno]";
+
+        NodeList nodeList = null;
+
+        try {
+            nodeList = (NodeList) xPath.compile(expression).evaluate(src, XPathConstants.NODESET);
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+
+        return conversion(nodeList);
     }
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        Document document = builder.parse(new FileInputStream("./src/emp-hier.xml"));
+        Document document = builder.parse(new FileInputStream("./src/emp.xml"));
 
         XPathCaller tst = new XPathCallerImpl();
-        Element[] ret = tst.getEmployees(document, "30", "olol");
-//        for(Element i: ret)
-//            System.out.println(i.getAttribute("empno"));
-        System.out.println(tst.getHighestPayed(document,"olol"));
+        Element[] ret = tst.getCoworkers(document, "7521","emp");
+
+        for(Element i: ret)
+            System.out.println(i.getAttribute("empno"));
+        System.out.println(tst.getHighestPayed(document,"10","olol"));
 
     }
 }
